3390e4c92058a212eedfa6d80fac4c119370128c
Joshua Holt <joshua.holt@libertymutual.com>
2017-09-15 11:22:48 -0700

    added ability to create new users

---
+++ src/main/java/com/theironyard/invoicify/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.theironyard.invoicify.controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.theironyard.invoicify.models.User;
+import com.theironyard.invoicify.models.UserRole;
+import com.theironyard.invoicify.repositories.UserRepository;
+
+@Controller
+@RequestMapping("/admin/users")
+public class UserController {
+    
+    private UserRepository userRepo;
+    
+    public UserController(UserRepository userRepo) {
+        this.userRepo = userRepo;
+    }
+    
+    @GetMapping("")
+    public ModelAndView list() {
+        ModelAndView mv = new ModelAndView("users/list");
+        mv.addObject("users", userRepo.findAll());
+        return mv;
+    }
+
+    @PostMapping("")
+    public ModelAndView create(User user, String role) {
+        ModelAndView mv = new ModelAndView("redirect:/admin/users");
+        User newUser = new User();
+        UserRole newRole = new UserRole(role, newUser);
+        List<UserRole> roles = new ArrayList<UserRole>();
+        newUser.setUsername(user.getUsername());
+        newUser.setPassword(user.getPassword());
+        roles.add(newRole);
+        newUser.setRoles(roles);
+        userRepo.save(newUser);
+        return mv;
+    }
+}
